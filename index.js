28444ab7d3341c6c2c339bdd409fffdff99bbf95
gwiyomiamy <octipuss@gmail.com>
2024-04-20 19:15:10 -0700

    questions created, README template coded, instructions to write README file coded

--- index.js
+++ index.js
@@ -1,7 +1,108 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const askQuestions = inquirer.prompt(
+[
+   {
+      type: "input",
+      message: "What is your project title?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Give a description for your project.",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "What is the installation process?",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "How does one use this app?",
+      name: "usage"
+    },
+    {
+      type: "list",
+      message: "What kind of liscense does this project have?",
+      name: "license",
+      choices: ["MIT"]
+    },
+   
+    {
+      type: "input",
+      message: "If you wrote any tests for your application, how can people run those tests?",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email"
+    }
+])
+.then((response) => {
+   const md = `
+   # ${response.title}
+   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+   ## Description
+   
+   ${response.description}
+   
+   ## Table of Contents 
+   
+   1. [Installation](#install)
+   2. [Usage](#usage)
+   3. [License](#license)
+   4. [How to Contribute](#contribute)
+   5. [Tests](#tests)
+   6. [Questions](#questions)
+   
+   <a id="install"></a>
+   ## Installation
+   
+   ${response.installation}
+   
+   <a id="usage"></a>
+   ## Usage
+   
+   ${response.usage}
+   
+   <a id="license"></a>
+   ## License
+   
+   ${response.license}
+
+   <a id="contribute"></a>
+   ## How to Contribute
+
+   (https://www.contributor-covenant.org/)
+
+   <a name="tests"></a>
+   ## Tests
+
+   ${response.tests}
+
+   <a name="questions"></a>
+   ## Questions
+
+   For any additional questions, please contact me:
+   github.com/${response.username}
+   ${response.email}
+   `
+   fs.writeFile("README.md", md, function(err){
+      if (err) return console.log("Error")
+      console.log("the README is ready")
+    })
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
